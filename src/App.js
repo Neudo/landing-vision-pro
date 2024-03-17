32392e85fd54f89620810e2db840b4e0269aba4a
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-03-17 13:56:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import { Canvas } from '@react-three/fiber';
+import { getProject } from '@theatre/core';
+import studio from '@theatre/studio';
+import extension from '@theatre/r3f/dist/extension';
+import { editable as e, SheetProvider } from "@theatre/r3f";
+import { PerspectiveCamera } from "@theatre/r3f";
+import demoProjectState from './state.json'
+import {OrbitControls} from "@react-three/drei";
+
+studio.initialize();
+studio.extend(extension)
+
+const demoSheet = getProject('Demo Project', { state: demoProjectState  }).sheet('Demo Sheet')
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+    useEffect(() => {
+        demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: 1, range: [0, 10] }))
+    }, [])
+
+
+    return (
+        <Canvas
+            style={{ height: '100vh', width: '100%' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            <SheetProvider sheet={demoSheet}>
+                <PerspectiveCamera theatreKey="Camera" makeDefault position={ [5,5,-5] } fov={75} />
+                <OrbitControls />
+                <ambientLight />
+                <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+                <e.mesh theatreKey="Cube">
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="hotpink" />
+                </e.mesh>
+            </SheetProvider>
+        </Canvas>
+    );
 }
 
 export default App;
