32bc29955fc39c04c447cdaf0233eca1c90447bb
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-03-19 14:02:06 +0100

    animation text main title

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { getProject } from '@theatre/core';
 import studio from '@theatre/studio';
@@ -7,49 +7,99 @@ import { editable as e, SheetProvider } from "@theatre/r3f";
 import { PerspectiveCamera } from "@theatre/r3f";
 import demoProjectState from './state.json'
 import visionRightState from './state-vision-right.json'
-import {Environment, OrbitControls} from "@react-three/drei";
+import {Environment} from "@react-three/drei";
 import {VisionPro} from "./models/VisionPro";
 import Header from "./components/Layout/Header";
 import { useControls } from 'leva'
-
-
 
 studio.initialize();
 studio.extend(extension)
 
 const demoSheet = getProject('Demo Project', { state: demoProjectState  }).sheet('Demo Sheet')
 const visionProRight = getProject('Vision Pro Right', { state: visionRightState  }).sheet('Vision Pro Right')
-
 document.body.classList.add('no-scroll');
+
 function App() {
-    const [alreadyPlayed, setAlreadyPlayed] = useState(false)
-
-
     const handleScrollOptions = () => {
         document.body.classList.remove('no-scroll');
         document.querySelector('span').classList.remove('hidden')
     }
+    const visionProRef = useRef()
+    const [visionProIsRotatable, setVisionProIsRotatable] = useState(false)
+    const visionProIsRotatableRef = useRef(visionProIsRotatable);
 
-    const moveVisionToPostionRight = () => {
+    useEffect(() => {
+        visionProIsRotatableRef.current = visionProIsRotatable;
+    }, [visionProIsRotatable]);
 
-        console.log(alreadyPlayed)
 
-            if(window.scrollY === 149){
-               visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2] }))
-                setAlreadyPlayed(true)
-            } else if(window.scrollY <= 148 && alreadyPlayed === true) {
-                console.log('else')
-                visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2], direction: 'reverse' }))
+    const rotateOnScroll = () => {
+        var scrollableElement = document.body;
+        scrollableElement.addEventListener('wheel', checkScrollDirection)
+
+        function checkScrollDirection(event) {
+            if(visionProIsRotatableRef.current){
+                setTimeout(() => {
+                    document.body.classList.remove('no-scroll');
+                }, 2000)
+                if (checkScrollDirectionIsUp(event)) {
+                    visionProRef.current.rotation.y -= 0.002;
+                } else {
+                    visionProRef.current.rotation.y += 0.002;
+                }
+            } else {
+                console.log('remove listener')
+                document.body.removeEventListener('wheel', checkScrollDirection)
             }
+        }
+
+        function checkScrollDirectionIsUp(event) {
+            if (event.wheelDelta) {
+                return event.wheelDelta > 0;
+            }
+            return event.deltaY < 0;
+        }
     }
 
-    window.addEventListener('scroll', moveVisionToPostionRight)
+
+    const moveVisionToPositionRight = () => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const intersecting = entry.isIntersecting
+                if(intersecting ){
+                    document.querySelector('.mainTitle').classList.remove('hide-text')
+                    setVisionProIsRotatable(true)
+                    document.body.classList.add('no-scroll');
+                    visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2] }).then(rotateOnScroll))
+                } else if(!intersecting) {
+                    document.querySelector('.mainTitle').classList.add('hide-text')
+                    setVisionProIsRotatable(false)
+                    visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, direction: 'reverse', rate: 1.5, range: [0, 1.8] }))
+                }
+            })
+        })
+        observer.observe(document.querySelector('.container'))
+
+
+        const observerSecondaryTitle = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                const intersecting = entry.isIntersecting
+                if(intersecting ){
+                    document.querySelector('.mainTitle').classList.remove('hide-text')
+                } else if(!intersecting) {
+                    document.querySelector('.mainTitle').classList.add('hide-text')
+                }
+            })
+        })
+        observerSecondaryTitle.observe(document.querySelector('.secondaryTitle'))
+    }
 
     useEffect(() => {
         setTimeout(() => {
             handleScrollOptions()
         }, 4000)
         demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: 1, range: [0, 5] }))
+        moveVisionToPositionRight()
     }, [])
 
     const { position, rotationY, scale } = useControls({
@@ -64,27 +114,28 @@ function App() {
         <>
             <Header/>
             <Canvas
-                style={{ height: '100vh', width: '100%', position: "fixed" }}
+                style={{height: '100vh', width: '100%', position: "fixed"}}
             >
-                {/*<OrbitControls />*/}
                 <Environment
-                    preset="apartment" />
+                    preset="apartment"/>
                 <SheetProvider sheet={demoSheet}>
-                    <PerspectiveCamera theatreKey="Camera" makeDefault position={ [-8,-.8,6.8] } fov={75} />
-                    <ambientLight />
-                    <e.pointLight theatreKey="Light" position={[10, 10, 10]} />
+                    <PerspectiveCamera theatreKey="Camera" makeDefault position={[-8, -.8, 6.8]} fov={75}/>
+                    <ambientLight/>
+                    <e.pointLight theatreKey="Light" position={[10, 10, 10]}/>
                 </SheetProvider>
                 <SheetProvider sheet={visionProRight}>
-                    <e.group theatreKey="VisionPro">
-                        <VisionPro scale={scale} rotation-y={rotationY} position={ position } />
+                    <e.group ref={visionProRef} theatreKey="VisionPro">
+                        <VisionPro scale={scale} rotation-y={rotationY} position={position}/>
                     </e.group>
                 </SheetProvider>
 
             </Canvas>
 
+            <h2 className="mainTitle hide-text">Welcome to the era of spatial computing.</h2>
+
+
             <div className="container">
-                <h2>Welcome to the era of spatial computing.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
+                <h2 className="secondaryTitle">Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
                 <h2>You navigate simply by using your eyes, hands, and voice.</h2>
             </div>
 
