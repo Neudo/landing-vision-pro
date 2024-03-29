a4587e82bb8f9f2b4d828522b30ac6dd9ec1a2c2
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-03-29 10:39:07 +0100

    pin ok

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { editable as e, SheetProvider } from "@theatre/r3f";
 import { PerspectiveCamera } from "@theatre/r3f";
 import demoProjectState from './state.json'
 import visionRightState from './state-vision-right.json'
-import {Environment, OrbitControls} from "@react-three/drei";
+import {Environment} from "@react-three/drei";
 import {VisionPro} from "./models/VisionPro";
 import Header from "./components/Layout/Header";
 import ContainerText from "./components/ContainerText";
@@ -15,80 +15,82 @@ import ContainerText from "./components/ContainerText";
 studio.initialize();
 studio.extend(extension)
 
-const demoSheet = getProject('Demo Project', { state: demoProjectState  }).sheet('Demo Sheet')
-const visionProRight = getProject('Vision Pro Right', { state: visionRightState  }).sheet('Vision Pro Right')
-document.body.classList.add('no-scroll');
+// const demoSheet = getProject('Demo Project', { state: demoProjectState  }).sheet('Demo Sheet')
+// const visionProRight = getProject('Vision Pro Right', { state: visionRightState  }).sheet('Vision Pro Right')
+// document.body.classList.add('no-scroll');
 
 function App() {
-    const handleScrollOptions = () => {
-        document.body.classList.remove('no-scroll');
-        document.querySelector('span').classList.remove('hidden')
-    }
-    const visionProRef = useRef()
-    const [visionProIsRotatable, setVisionProIsRotatable] = useState(false)
-    const visionProIsRotatableRef = useRef(visionProIsRotatable);
+    // const handleScrollOptions = () => {
+    //     document.body.classList.remove('no-scroll');
+    //     document.querySelector('span').classList.remove('hidden')
+    // }
+    // const visionProRef = useRef()
+    // const [visionProIsRotatable, setVisionProIsRotatable] = useState(false)
+    // const visionProIsRotatableRef = useRef(visionProIsRotatable);
+    //
+    // useEffect(() => {
+    //     visionProIsRotatableRef.current = visionProIsRotatable;
+    // }, [visionProIsRotatable]);
+    //
 
-    useEffect(() => {
-        visionProIsRotatableRef.current = visionProIsRotatable;
-    }, [visionProIsRotatable]);
+    // const rotateOnScroll = () => {
+    //     console.log('on est dans rotateOnScroll')
+    //     let scrollableElement = document.body;
+    //     scrollableElement.addEventListener('wheel', checkScrollDirection)
+
+    //     function checkScrollDirection(event) {
+    //         if(visionProIsRotatableRef.current){
+    //             if (checkScrollDirectionIsUp(event)) {
+    //                 visionProRef.current.rotation.y -= 0.002;
+    //             } else {
+    //                 visionProRef.current.rotation.y += 0.002;
+    //             }
+    //         } else {
+    //             document.body.removeEventListener('wheel', checkScrollDirection)
+    //         }
+    //     }
+    //
+    //     function checkScrollDirectionIsUp(event) {
+    //         if (event.wheelDelta) {
+    //             return event.wheelDelta > 0;
+    //         }
+    //         return event.deltaY < 0;
+    //     }
+    // }
 
 
-    const rotateOnScroll = () => {
-        let scrollableElement = document.body;
-        scrollableElement.addEventListener('wheel', checkScrollDirection)
-
-        function checkScrollDirection(event) {
-            if(visionProIsRotatableRef.current){
-                if (checkScrollDirectionIsUp(event)) {
-                    visionProRef.current.rotation.y -= 0.002;
-                } else {
-                    visionProRef.current.rotation.y += 0.002;
-                }
-            } else {
-                document.body.removeEventListener('wheel', checkScrollDirection)
-            }
-        }
-
-        function checkScrollDirectionIsUp(event) {
-            if (event.wheelDelta) {
-                return event.wheelDelta > 0;
-            }
-            return event.deltaY < 0;
-        }
-    }
-
-
-    const moveVisionToPositionRight = () => {
-        const observer = new IntersectionObserver(entries => {
-            entries.forEach(entry => {
-                const intersecting = entry.isIntersecting
-                if(intersecting ){
-                    document.querySelector('.mainTitle').classList.remove('hide-text')
-                    setVisionProIsRotatable(true)
-                    document.body.classList.add('no-scroll');
-                    if(document.body.classList.contains('no-scroll')){
-                        setTimeout(() => {
-                            document.body.classList.remove('no-scroll');
-                        }, 2000)
-                    }
-                    visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2] }).then(rotateOnScroll))
-                } else if(!intersecting) {
-                    document.querySelector('.mainTitle').classList.add('hide-text')
-                    setVisionProIsRotatable(false)
-                    visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, direction: 'reverse', rate: 1.5, range: [0, 1.8] }))
-                }
-            })
-        })
-        observer.observe(document.querySelector('.container'))
-    }
-
-    useEffect(() => {
-        setTimeout(() => {
-            handleScrollOptions()
-        }, 4000)
-        demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: 1, range: [0, 5] }))
-        moveVisionToPositionRight()
-    }, [])
+    // const moveVisionToPositionRight = () => {
+    //     const observer = new IntersectionObserver(entries => {
+    //         entries.forEach(entry => {
+    //             const intersecting = entry.isIntersecting
+    //             if(intersecting ){
+    //                 console.log('on voit wrapper lol')
+    //                 // document.querySelector('.mainTitle').classList.remove('hide-text')
+    //                 setVisionProIsRotatable(true)
+    //                 document.body.classList.add('no-scroll');
+    //                 if(document.body.classList.contains('no-scroll')){
+    //                     setTimeout(() => {
+    //                         document.body.classList.remove('no-scroll');
+    //                     }, 2000)
+    //                 }
+    //                 visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2] }).then(rotateOnScroll))
+    //             } else if(!intersecting) {
+    //                 // document.querySelector('.mainTitle').classList.add('hide-text')
+    //                 setVisionProIsRotatable(false)
+    //                 visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, direction: 'reverse', rate: 1.5, range: [0, 1.8] }))
+    //             }
+    //         })
+    //     })
+    //     observer.observe(document.querySelector('.container'))
+    // }
+    //
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         handleScrollOptions()
+    //     }, 4000)
+    //     demoSheet.project.ready.then(() => demoSheet.sequence.play({ iterationCount: 1, range: [0, 5] }))
+    //     moveVisionToPositionRight()
+    // }, [])
 
 
     return (
@@ -96,24 +98,24 @@ function App() {
         // Scene
         <>
             <Header/>
-            <Canvas
-                style={{height: '100vh', width: '100%', position: "fixed"}}
-            >
-                {/*<OrbitControls/>*/}
-                <Environment
-                    preset="apartment"/>
-                <SheetProvider sheet={demoSheet}>
-                    <PerspectiveCamera theatreKey="Camera" makeDefault position={[-8, -.8, 6.8]} fov={75}/>
-                    <ambientLight/>
-                    <e.pointLight theatreKey="Light" position={[10, 10, 10]}/>
-                </SheetProvider>
-                <SheetProvider sheet={visionProRight}>
-                    <e.group ref={visionProRef} theatreKey="VisionPro">
-                        <VisionPro scale={20} rotation-y={1.02} position={[0, 0, 0]}/>
-                    </e.group>
-                </SheetProvider>
+            {/*<Canvas*/}
+            {/*    style={{height: '100vh', width: '100%', position: "fixed"}}*/}
+            {/*>*/}
 
-            </Canvas>
+            {/*    <Environment*/}
+            {/*        preset="apartment"/>*/}
+            {/*    <SheetProvider sheet={demoSheet}>*/}
+            {/*        <PerspectiveCamera theatreKey="Camera" makeDefault position={[-8, -.8, 6.8]} fov={75}/>*/}
+            {/*        <ambientLight/>*/}
+            {/*        <e.pointLight theatreKey="Light" position={[10, 10, 10]}/>*/}
+            {/*    </SheetProvider>*/}
+            {/*    <SheetProvider sheet={visionProRight}>*/}
+            {/*        <e.group ref={visionProRef} theatreKey="VisionPro">*/}
+            {/*            <VisionPro scale={20} rotation-y={1.02} position={[0, 0, 0]}/>*/}
+            {/*        </e.group>*/}
+            {/*    </SheetProvider>*/}
+
+            {/*</Canvas>*/}
             <ContainerText/>
 
 
--- src/components/ContainerText.js
+++ src/components/ContainerText.js
@@ -1,11 +1,8 @@
 import React, {useEffect, useRef} from 'react';
-import gsap from "gsap";
-import { useGSAP,  } from "@gsap/react";
+import {gsap} from "gsap";
+
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
-
-gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 
@@ -13,30 +10,37 @@ function ContainerText() {
     const container = useRef();
 
     useEffect(() => {
-        gsap.to(".container", {
-            opacity: 1,
-            scrollTrigger: {
-                trigger: ".container",
-                start: "top 50%",
-                end: "10% 30%",
-                markers: true,
-                once: false,
-            }
-        })
+        let ctx = gsap.context(() => {
+            gsap.to(".page", {
+                scrollTrigger: {
+                    trigger: ".page",
+                    start: "top top",
+                    end: "bottom 30%",
+                    pin: true,
+                    pinSpacing: false,
+                    markers: true
+                }
+            });
+        }, container); // <- selector scoping
+        return () => ctx.revert();
     }, [])
 
     return (
+        <div ref={container} className="App">
 
-        <>
-            <h1 className="mainTitle hide-text">Welcome to the era of spatial computing.</h1>
-            <div ref={container} className="container">
+            <div className="separator"></div>
+            <div className="page">
+                <h1>Welcome to the era of spatial computing.</h1>
+            </div>
 
-                <h2 className="secondaryTitle">Apple Vision Pro seamlessly blends digital content with your physical
-                    space.</h2>
+            <div className="page2">
+                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
+            </div>
+
+            <div className="page3">
                 <h2>You navigate simply by using your eyes, hands, and voice.</h2>
             </div>
-        </>
-
+        </div>
     );
 }
 
