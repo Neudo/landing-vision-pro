30c86c28fb7031f3d3ece5686cf6feb0118db71d
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-03-31 18:27:14 +0200

    titles ok

--- src/components/ContainerText.js
+++ src/components/ContainerText.js
@@ -9,39 +9,77 @@ gsap.registerPlugin(ScrollTrigger);
 function ContainerText() {
     const container = useRef();
 
+
+
     useEffect(() => {
+        const titles = document.querySelectorAll('h2')
         let ctx = gsap.context(() => {
-            gsap.to(".page", {
+
+            let tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: ".page",
-                    start: "top 30%",
-                    end: "bottom 60%",
+                    start: "top 40%",
+                    end: "bottom 57%",
                     pin: true,
                     pinSpacing: false,
-                    markers: true
+                    markers: false,
+                    onEnter: () => {
+                        setTimeout(() => {
+                            document.querySelector('h1').classList.remove('hide-text')
+                        }, 400)
+                    },
+                    onLeaveBack: () => {
+                        setTimeout(() => {
+                            document.querySelector('h1').classList.add('hide-text')
+                        }, 400)
+                    },
+                    onEnterBack: () => {
+                        setTimeout(() => {
+                            document.querySelector('h1').classList.remove('hide-text')
+                        }, 400)
+                    },
                 }
             });
+
+            tl.to(".page", {}).pause()
+
+            titles.forEach((title, index) => {
+
+                gsap.to(title, {
+                    opacity: 1,
+                    scale: 1,
+                    duration: .4,
+                    ease: "power3.out",
+                    scrollTrigger: {
+                        trigger: title,
+                        start: "top 90%",
+                        markers: true,
+                        once: false,
+                    },
+                })
+            })
         }, container); // <- selector scoping
         return () => ctx.revert();
     }, [])
 
+
+
     return (
-        <div ref={container} className="container">
-
-            <div className="separator"></div>
-            <div className="page">
-                <h1 className="mainTitle" >Welcome to the era of spatial computing.</h1>
+        <>
+            <div ref={container} className="container">
+                <div className="page">
+                    <h1 className="mainTitle hide-text" >Welcome to the era of spatial computing.</h1>
+                </div>
+                <div className="page2">
+                    <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
+                    <h2>You navigate simply by using your eyes, hands, and voice.</h2>
+                    <h2>So you can do the things you love in ways never before possible.</h2>
+                    <h2>You’ve never seen everything like this before.</h2>
+                </div>
             </div>
 
-            <div className="page2">
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-                <h2>Apple Vision Pro seamlessly blends digital content with your physical space.</h2>
-            </div>
-        </div>
+            <div className="finalRoom"></div>
+        </>
     );
 }
 
