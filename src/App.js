22c71ee920d423dc22f5f290775a5509230a4d5d
subskill-quentin <91881904+subskill-quentin@users.noreply.github.com>
2024-03-31 18:38:03 +0200

    fix: remove anime bottom of the page

--- src/App.js
+++ src/App.js
@@ -62,7 +62,7 @@ function App() {
         const observer = new IntersectionObserver((entries, observer) => {
             entries.forEach(entry => {
                 const intersecting = entry.isIntersecting
-                if(intersecting ){
+                if(intersecting && window.scrollY < 150 ){
                     setVisionProIsRotatable(true)
                     document.body.classList.add('no-scroll');
                     if(document.body.classList.contains('no-scroll')){
@@ -72,8 +72,15 @@ function App() {
                     }
                     visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, range: [0, 2] }).then(rotateOnScroll))
                 } else if(!intersecting){
+                    if(window.scrollY < 150){
+                        console.log('on est pas là en base de la page')
+                        console.log('scrollY', window.scrollY)
                     setVisionProIsRotatable(false)
                     visionProRight.project.ready.then(() => visionProRight.sequence.play({ iterationCount: 1, direction: 'reverse', rate: 1.5, range: [0, 1.8] }))
+                    } else {
+                        console.log('on est ici en bas de la page')
+                        console.log('scrollY else', window.scrollY)
+                    }
                 }
             })
         })
